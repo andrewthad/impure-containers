534c2b8721162331c0c0f7fa060a026483c0e8f5
Andrew Martin <andrew.thaddeus@gmail.com>
2017-11-04 08:25:06 -0400

    make it build with new and old GHC

--- src/Data/Primitive/Array/Maybe.hs
+++ src/Data/Primitive/Array/Maybe.hs
@@ -11,7 +11,7 @@ module Data.Primitive.Array.Maybe
 import Control.Monad.Primitive
 import Data.Primitive.Array
 import GHC.Prim (reallyUnsafePtrEquality#)
-import GHC.Types (Any)
+import GHC.Exts (Any)
 import Unsafe.Coerce (unsafeCoerce)
 
 newtype MutableMaybeArray s a = MutableMaybeArray (MutableArray s Any)
